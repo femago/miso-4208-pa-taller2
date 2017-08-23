30823f3ea5d43492f8dcd737478593ce96ab23ec
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-08-22 22:53:47 -0500

    nightwatch

---
+++ nightwatchjs/tests/buscar-profesor.js
@@ -0,0 +1,14 @@
+module.exports = { // adapted from: https://git.io/vodU0
+  'Buscar Profesor': function (browser) {
+    browser
+      .url('https://losestudiantes.co/')
+      .click('.botonCerrar')
+      .waitForElementVisible('.botonIngresar', 4000)
+      .setValue('.Select-input input', 'Mario Linares Vasquez')
+      .waitForElementPresent('.Select-option', 2000)
+      .click('.Select-menu-outer .Select-menu .Select-option')
+      .waitForElementPresent('h1.nombreProfesor', 2000)
+      .assert.containsText('h1.nombreProfesor', 'Mario Linares Vasquez')
+      .end();
+  }
+};
